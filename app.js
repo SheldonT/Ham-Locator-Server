51aebd9a29048e03f536fb34006d90524ee4b3c0
Patrick Costello <31824057+prcostello@users.noreply.github.com>
2023-02-02 16:23:26 -0400

    week 2

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-/** @format */
-
 const express = require("express");
+const { config } = require('dotenv');
+
+// Init app and config
 const app = express();
-const port = 3005;
+config();
+
+const users = require('./controllers/users.controller.ts')
+
+// Needed to receive JSON
+app.use(express.json({limit: "1mb"}))
+
 
 app.get("/", (req, res) => {
-  res.send("Hello World!!!");
+  // code here
 });
+// Define new 'base' route, appens users
+app.use('/users', users)
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
 });
