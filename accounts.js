a650e696d2799db4b0956546856101012d62b7e4
Sheldon Tower <towersheldon@gmail.com>
2023-02-03 07:59:38 -0330

    creating auth route and endpoint

---
+++ accounts.js
@@ -0,0 +1,9 @@
+/** @format */
+
+module.exports.user = {
+  sheldon: {
+    id: "1",
+    username: "sheldon",
+    email: "test@gmail.com",
+  },
+};
--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 /** @format */
 
 const express = require("express");
-const app = express();
-const port = 3005;
+const cors = require("cors");
+const { config } = require("dotenv");
+const data = require("./accounts.js");
+const jdata = require("./accounts.json");
 
-app.get("/", (req, res) => {
-  res.send("Hello World!!!");
+config();
+
+const app = express();
+
+app.use(express.json({ limit: "1mb" }));
+app.use(cors());
+
+app.get("/auth", cors(), (req, res) => {
+  console.log(req.body.data);
+  //console.log(data);
+  res.send(jdata.sheldon);
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+/*app.post("/", (req, res) => {
+  console.log(req.body.data.name);
+  res.send("Post");
+});*/
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
 });
