ff97362e1d784c7d17f14b07b6710735b78b1287
Patrick Costello <31824057+prcostello@users.noreply.github.com>
2023-02-07 17:07:01 -0400

    some TS stuff

--- controllers/users.controller.ts
+++ controllers/users.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response, Router } from "express"
 
-export const UsersController = () => {
+export const UsersController = (): Router => {
     const routes = Router();
 
     routes.get('/', (req: Request, res: Response) => {
