db08c339a676485de76cf46f60895b5274013ebb
Patrick Costello <31824057+prcostello@users.noreply.github.com>
2023-02-12 12:28:47 -0400

    intro to typedoc

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,6 +1,21 @@
 export type User = {
+    /**
+     * Primary Key of User
+     */
     id: string,
+
+    /**
+     * Email address of user
+     */
     email: string,
+
+    /**
+     * Name of user
+     */
     name: string,
+
+    /**
+     * Hashed password stored for user
+     */
     password: string,
 }
