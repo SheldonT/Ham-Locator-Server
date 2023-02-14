94d716d018ba8edb43cd27f7a974562f7dd2823f
Patrick Costello <31824057+prcostello@users.noreply.github.com>
2023-02-14 11:24:56 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,19 @@ async function start(){
   const app: Express = express();
   const port = process.env.PORT;
 
+  /**
+   * Wanted to add some thoughts here before we integrate MySQL into this project
+   * 
+   * We can either establish connection in a loader and .on('close', ...) of the app
+   * we could close that connection
+   * 
+   * OR
+   * 
+   * Everytime we utilize the DB class/functions we can establish connection and close connection
+   * with each use.
+   * 
+   */
+  
   ExpressLoader(app);
 
   app.listen(port, () => {
